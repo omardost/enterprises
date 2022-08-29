c7f9e16e2c448d90dda36921805b8f2c3de9083d
Omar Soriano <dsoriano@ec.krugercorp.com>
2022-08-29 17:04:46 -0500

    first commit

---
+++ src/main/java/com/enterprises/administrate/AdministrateApplication.java
@@ -0,0 +1,13 @@
+package com.enterprises.administrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministrateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enterprises/administrate/AdministrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprises.administrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
